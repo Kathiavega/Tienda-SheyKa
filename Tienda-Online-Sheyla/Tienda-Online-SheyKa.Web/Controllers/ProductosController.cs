71d893ac7d4e974f85f250cd4359c0e1eeb4e4a6
Kathia Vega <vegakathia6@gmail.com>
2021-07-05 14:31:06 -0600

    Crear validaciones de sus mantenimientos y agregar imagenes

---
+++ Tienda-Online-Sheyla/Tienda-Online-SheyKa.Web/Controllers/ProductosController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tienda_Online_SheyKa.BL;
+
+namespace Tienda_Online_SheyKa.Web.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult Index()
+        {
+            var productosBL = new ProductosBL();
+            var listadeProductos = productosBL.ObtenerProductos();
+
+            return View(listadeProductos);
+        }
+    }
+}
